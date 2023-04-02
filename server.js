e691babd6594310d6e4f9f37544ba71fbaa8351e
Sahil <Sahillohan07@gmail.com>
2023-04-02 22:51:03 +0530

    Added Get request handlers

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.get("/",function(req,res){
     res.render('home.ejs');
 })
 
+app.get("/contact",function(req,res){
+    res.render('contact.ejs');
+})
+
+app.get("/about",function(req,res){
+    res.render('about.ejs');
+})
+
+app.get("/compose",function(req,res){
+    res.render('compose.ejs');
+})
+
+
 app.listen(3000,function(){
     console.log("server started at port 3000\n");
 })
